9afc9f35c1c3d99fd6cf97030cb3cf469b488ab0
Joel Lagoria <lagoriajoel3413@gmai.com>
2022-07-15 18:35:39 -0300

    primer commit

---
+++ JS/miarchivo.js
@@ -0,0 +1,7 @@
+
+const primerNumero =parseInt(prompt("ingrese la primer Nota"))
+const segundoNumero=parseInt(prompt("ingrese segundo nota"))
+const tercerNro=parseInt(prompt("ingrese tercerNro nota"))
+
+const promedio= (primerNumero*segundoNumero*tercerNro)/3
+console.log(`el promedio: ${promedio}`);
