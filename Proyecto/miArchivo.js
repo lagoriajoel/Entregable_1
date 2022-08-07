15269168400f6bb90e631e59bc08eea650b6e4ff
Joel Lagoria <lagoriajoel3413@gmai.com>
2022-08-07 11:11:03 -0300

    proyecto: utilizando el DOM

--- Proyecto/miArchivo.js
+++ Proyecto/miArchivo.js
@@ -1,98 +1,9 @@
-//const  hamb=prompt("seleccione una hamburgesa (ingrese un numero del 1 al 4)");
-
-
-   //function eleccionMenu() {
-
-   
-  
-//}
+import { menu } from "./productos.js";
 
 
 const CARRITO = [];
 
-   const menu = [
-                  {
-                     nombre: "hamburgesa Clasica",
-                     precio: 800,
-                     ingredientes : ["lechuga", "mayonesa", "tomate","queso","huevo","papas fritas"],
-                     tipo: "simple"
-                  },
-                  
-                  {
-                     nombre: "hamburgesa chesse",
-                     precio: 900,
-                    ingredientes : ["chedar x2", "mayonesa", "cebolla en cubos","ketchup","papas fritas"],
-                    tipo: "simple"
-
-                  },
-                  {
-                     nombre: "hamburgesa yankke",
-                     precio: 1000,
-                    ingredientes : ["chedar x2", "bacon x2", "pepinillos","barbacoa","papas fritas"],
-                    tipo: "simple"
-
-
-                  },
-                  {
-                     nombre: "hamburgesa criolla",
-                     precio: 1200,
-                    ingredientes : ["mayonesa de apio", "chimichurro", "mollejas al limon","papas fritas"],
-                    tipo: "simple"
-
-
-
-                  },
-                  {
-                     nombre: "hamburgesa Mexicana",
-                     precio: 1200,
-                    ingredientes : ["chedar x2", "bacon x2", "guacamole","mayonesa ranchera","papas fritas"],
-                    tipo: "simple"
-
-
-                  },
-                  {
-                     nombre: "hamburgesa Clasica MAX",
-                     precio: 1800,
-                     ingredientes : ["lechuga", "mayonesa", "tomate","queso","huevo","papas fritas"],
-                     tipo: "doble"
-                  },
-                  
-                  {
-                     nombre: "hamburgesa chesse MAX",
-                     precio: 1900,
-                    ingredientes : ["chedar x2", "mayonesa", "cebolla en cubos","ketchup","papas fritas"],
-                    tipo: "doble"
-
-                  },
-                  {
-                     nombre: "hamburgesa yankke MAX",
-                     precio: 1800,
-                    ingredientes : ["chedar x2", "bacon x2", "pepinillos","barbacoa","papas fritas"],
-                    tipo: "doble"
-
-
-
-                  },
-                  {
-                     nombre: "hamburgesa criolla MAX",
-                     precio: 1800,
-                    ingredientes : ["mayonesa de apio", "chimichurro", "mollejas al limon","papas fritas"],
-                    tipo: "doble"
-
-
-
-
-                  },
-                  {
-                     nombre: "hamburgesa Veggie",
-                     precio: 1200,
-                    ingredientes : ["chedar", "pepinillos", "mayonesa de apio","papas fritas"],
-                    tipo: "vegetariana"
-
-
-                  }
-   
-                ]
+  
 
         function tomarPedido(){
 
@@ -190,30 +101,30 @@ const CARRITO = [];
          //console.log(CARRITO);
          // Pre entrega Nro 1 : utilizacion de metodos de busqueda y filtrado
           //pedimos al usuario que seleccione un tipo de hamburgesa para el filtrado
-         const seleccion = prompt(`selecciono un tipo de menu:                  
-                           1. hamburgesas simples.
-                           2. hamburguesas Dobles.
-                           3. hamburgesas Vegeterianas.`)
+         // const seleccion = prompt(`selecciono un tipo de menu:                  
+         //                   1. hamburgesas simples.
+         //                   2. hamburguesas Dobles.
+         //                   3. hamburgesas Vegeterianas.`)
 
     
        // segun la opcion elegida llamamos al metodo filtraTipo que devuelve un el menu con la seleccion
         
-        let tipo="";
-         if (seleccion==="1") {                                             
-          tipo="simple"  
-          filtrarTipo(tipo)      
+      //   let tipo="";
+      //    if (seleccion==="1") {                                             
+      //     tipo="simple"  
+      //     filtrarTipo(tipo)      
            
-        }
-        else if (seleccion==="2") {
+      //   }
+      //   else if (seleccion==="2") {
          
-         tipo="doble"  
-          filtrarTipo(tipo)   
+      //    tipo="doble"  
+      //     filtrarTipo(tipo)   
       
-        }
-        else if (seleccion==="3") {
-         tipo="vegetariana" 
-         filtrarTipo(tipo)   
-        }
+      //   }
+      //   else if (seleccion==="3") {
+      //    tipo="vegetariana" 
+      //    filtrarTipo(tipo)   
+      //   }
 
 
         // capturamos el resultado en "tipo" y se realiza la busqueda con filter y recorremos el array que nos devuelve
@@ -228,22 +139,20 @@ const CARRITO = [];
         //mostrar los elementos en pantalla
         const mostrarProductos =(menu)=>{
 
-        const contenedor = document.getElementById("contenedor");
+        const contenedor = document.getElementById("cards");
 
-        menu.forEach((menu) => {
+          menu.forEach((menu) => {
 
          const div = document.createElement("div");
          div.classList.add("card");
-         div.innerHTML +=`<div class="card image>
-                              <img src=${menu.img}>
-                              <span class="card-title">${menu.nombre}</span>
-                              <a class="card-link" id=boton${menu.id}></a>
-                          </div>
-                          <div class="card-content">
-                             <p>${menu.description}</p>
-                             <p>${menu.precio}</p>
-                          
-                          </div>         
+         div.innerHTML +=`<div class="container">
+                                 <img src=${menu.img} alt="chesse">
+                           
+                                 <div class="details">
+                                 <h3>${menu.nombre}</h3>
+                                 <p>${menu.ingredientes.join()} </p>
+                                 </div>
+                           </div>
                          `
         contenedor.appendChild(div)
         }
@@ -251,6 +160,7 @@ const CARRITO = [];
       
 
         )};
+        mostrarProductos(menu);
        
 
 
