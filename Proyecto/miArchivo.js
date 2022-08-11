492414f8a523c7efaaf095332e639b884b4bf737
Joel Lagoria <lagoriajoel3413@gmai.com>
2022-08-10 21:34:02 -0300

    funcionalidad del boton modo oscuro

--- Proyecto/miArchivo.js
+++ Proyecto/miArchivo.js
@@ -162,8 +162,20 @@ const CARRITO = [];
         )};
         mostrarProductos(menu);
        
-
-
+       // funcion dark mode
+    
+let darkmode = document.querySelector('#darkmode');
+ 
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+ 
 
         
 
