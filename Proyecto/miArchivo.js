3f2528346bbb0b6d1c55921acf01d719bb8c63c7
Joel Lagoria <lagoriajoel3413@gmai.com>
2022-08-17 22:30:15 -0300

    segunda entrega del Proyecto final

--- Proyecto/miArchivo.js
+++ Proyecto/miArchivo.js
@@ -1,184 +1,268 @@
-import { menu } from "./productos.js";
 
 
-const CARRITO = [];
 
-  
+            const menu = {
+                datos: [
+                  {
+                    id:1, 
+                    nombre: "hamburgesa Clasica",
+                     precio: 800,
+                     ingredientes : ["lechuga", "mayonesa", "tomate","queso","huevo","papas fritas"],
+                     tipo: "simple",
+                     img: 'images/clasica.jpg'
+                  },
+                  
+                  {
+                    id:2, 
+                     nombre: "hamburgesa chesse",
+                     precio: 900,
+                    ingredientes : ["chedar x2", "mayonesa", "cebolla en cubos","ketchup","papas fritas"],
+                    tipo: "simple",
+                    img: 'images/chesse.jpg'
+              
+                  },
+                  {id:3, 
+                     nombre: "hamburgesa yankke",
+                     precio: 1000,
+                    ingredientes : ["chedar x2", "bacon x2", "pepinillos","barbacoa","papas fritas"],
+                    tipo: "simple",
+                    img: 'images/yankee.jpg'
+              
+              
+                  },
+                  {
+                    id:4, 
+                    nombre: "hamburgesa criolla",
+                    precio: 1200,
+                    ingredientes : ["mayonesa de apio", "chimichurro", "mollejas al limon","papas fritas"],
+                    tipo: "simple",
+                    img: "images/criolla.jpg"
+              
+              
+              
+                  },
+                  {
+                    id:5, 
+                    nombre: "hamburgesa Mexicana",
+                    precio: 1200,
+                    ingredientes : ["chedar x2", "bacon x2", "guacamole","mayonesa ranchera","papas fritas"],
+                    tipo: "simple",
+                    img: 'images/mexicana.jpg'
+              
+              
+                  },
+                  {
+                    id:6, 
+                     nombre: "hamburgesa Clasica MAX",
+                     precio: 1800,
+                     ingredientes : ["lechuga", "mayonesa", "tomate","queso","huevo","papas fritas"],
+                     tipo: "doble",
+                     img: 'images/doble.jpg'
+                  },
+                  
+                  {
+                    id:7, 
+                    nombre: "hamburgesa chesse MAX",
+                    precio: 1900,
+                    ingredientes : ["chedar x2", "mayonesa", "cebolla en cubos","ketchup","papas fritas"],
+                    tipo: "doble",
+                    img: 'images/doble.jpg'
+              
+                  },
+                  {
+                    id:8, 
+                    nombre: "hamburgesa yankke MAX",
+                    precio: 1800,
+                    ingredientes : ["chedar x2", "bacon x2", "pepinillos","barbacoa","papas fritas"],
+                    tipo: "doble",
+                    img: 'images/doble.jpg'
+              
+              
+              
+                  },
+                  {
+                    id:9, 
+                    nombre: "hamburgesa criolla MAX",
+                    precio: 1800,
+                    ingredientes : ["mayonesa de apio", "chimichurro", "mollejas al limon","papas fritas"],
+                    tipo: "doble",
+                    img: 'images/doble.jpg'
+              
+              
+              
+              
+                  },
+                  {
+                    id:10, 
+                    nombre: "hamburgesa Veggie",
+                    precio: 1200,
+                    ingredientes : ["chedar", "pepinillos", "mayonesa de apio","papas fritas"],
+                    tipo: "veggie",
+                    img: 'images/veggie.jpg'
+              
+              
+                  }
+              
+                ],}
+        
 
-        function tomarPedido(){
-
-                                 const eleccion = prompt(`
-                                 Por favor eliga el numero segun corresponda 
-                                 a su Hamburguesa :
-
-
-                                 1. Nombre: ${menu[0].nombre}
-                                 Precio: ${menu[0].precio}
-                                 Ingredientes: ${menu[0].ingredientes}
-
-                                 2. Nombre: ${menu[1].nombre}
-                                 Precio: $${menu[1].precio}
-                                 Ingredientes: ${menu[1].ingredientes}
-
-                                 3. Nombre: ${menu[2].nombre}
-                                 Precio: $${menu[2].precio}
-                                 Ingredientes: ${menu[2].ingredientes}
-
-                                 4. Nombre: ${menu[3].nombre}
-                                 Precio: $${menu[3].precio}
-                                 Ingredientes: ${menu[3].ingredientes}
-
-                                 5. Nombre: ${menu[4].nombre}
-                                 Precio: $${menu[4].precio}
-                                 Ingredientes: ${menu[4].ingredientes}
-
-                              `)
-
-                               mostrarEleccion(eleccion)  
-                               
-                        cantidadIngresada = prompt("Ingrese la cantidad");
-                               
-                                 
-
-                        const pedido= { nombre: menu[eleccion-1].nombre, precio: menu[eleccion-1].precio , ingredientes: menu[eleccion-1].ingredientes, cantidad: cantidadIngresada}
-                        CARRITO.push(pedido);
-
-                        seguirComprando = prompt("Queres seguir comprando ?? si/no").toLowerCase();
-
-                        if (seguirComprando === "si") {
-                            tomarPedido()
-                        } else {
-                            alert(` Gracias por su compra!!!
-                                   Total de la compra es : ${totalDeLaCompra()}`);
-                        }
-
-                     
-                      
+            for (let i of menu.datos) {
+                //creamos un card para mostrar el producto del menu
+                let card = document.createElement("div");
+              
+                //se agrega a las cards las clases "card","tipode hamburgesa",y "hide" para ocultarlas inicialmente
+                card.classList.add("card", i.tipo, "hide");
+                //cremaos un div imgcontainer
+                let imgContainer = document.createElement("div");
+                imgContainer.classList.add("image-container");
+                //img tag
+                let image = document.createElement("img");
+                image.setAttribute("src", i.img);
+                imgContainer.appendChild(image);
+                card.appendChild(imgContainer);
+                //container
+                let container = document.createElement("div");
+                container.classList.add("container");
+                //nombre producto
+                let name = document.createElement("h5");
+                name.classList.add("nombreProducto");
+                name.innerText = i.nombre.toUpperCase();
+                container.appendChild(name);
+                //precio
+                let price = document.createElement("h5");
+                price.innerText = "$" + i.precio;
+                container.appendChild(price);
+                card.appendChild(container);
+                document.getElementById("cards").appendChild(card);
+                //descripcion
+                let descripcion = document.createElement("h6");
+                descripcion.innerText =   i.ingredientes.join(" ");
+                container.appendChild(descripcion);
+                card.appendChild(container);
+                document.getElementById("cards").appendChild(card);
+                //botom addcarrito
+                let addCarrito = document.createElement("div");
+                addCarrito.innerHTML = `<button type='button' class='addcarrito' onclick=\"addCarrito(${i.id});\">Agregar al Carrito</button>`;
                 
-        }
-   
-        //tomarPedido()
-      
-        function totalDeLaCompra(){
+                container.appendChild(addCarrito);
+              
+                card.appendChild(container);
+                document.getElementById("cards").appendChild(card);
+              }
+              //
+              function filterProduct(value) {
+                //traemos los botones
+                let buttons = document.querySelectorAll(".btnFiltro");
+                buttons.forEach((button) => {
+                  //check si el valor es igual al texto
+                  if (value.toUpperCase() == button.innerText.toUpperCase()) {
+                    button.classList.add("active");
+                  } else {
+                    button.classList.remove("active");
+                  }
+                });
+                //seleccionamos todas las card
+                let elements = document.querySelectorAll(".card");
+                //iteramos
+                elements.forEach((element) => {
+                
+                  if (value == "todas") {
+                    element.classList.remove("hide");
+                  } else {
+                
+                    if (element.classList.contains(value)) {
+                   
+                      element.classList.remove("hide");
+                    } else {
+                    
+                      element.classList.add("hide");
+                    }
+                  }
+                });
+              }
+            
+              //mostramos inicialmente todos los productos
+              window.onload = () => {
+                filterProduct("todas");
+              };
 
-         let total = 0;
-         for(let i = 0; i < CARRITO.length; i++){
-             total += CARRITO[i].precio * parseInt(CARRITO[i].cantidad);
-         }
-         return total; }
+              // creamos un array para el carrito
+              carrito=[];
+              // funcion para agragar productos al carrito
+              function addCarrito(id){
+                let prod=menu.datos.find(elem => elem.id==id);
+                carrito.push(prod);
+                console.log(carrito);
+                localStorage.setItem("carrito", JSON.stringify(carrito));
 
-       
-         
-       
-        function mostrarEleccion(eleccion){
+              
+               
+               
+               
+              }
 
-         switch (eleccion) {
-         case "1": alert   (`selecciono ${menu[eleccion-1].nombre}
-            precio:${menu[eleccion-1].precio}
-            ingredientes:${menu[eleccion-1].ingredientes}`)
-            break;
-         case "2":  alert   (`selecciono ${menu[eleccion-1].nombre}
-            precio:${menu[eleccion-1].precio}
-            ingredientes:${menu[eleccion-1].ingredientes}`)
-            break;   
-         case "3": alert   (`selecciono ${menu[eleccion-1].nombre}
-            precio:${menu[eleccion-1].precio}
-            ingredientes:${menu[eleccion-1].ingredientes}`) 
-            break;
-         case "4":  alert   (`selecciono ${menu[eleccion-1].nombre}
-            precio:${menu[eleccion-1].precio}
-            ingredientes:${menu[eleccion-1].ingredientes}`)
-            break;
-         case "5":  alert   (`selecciono ${menu[eleccion-1].nombre}
-            precio:${menu[eleccion-1].precio}
-            ingredientes:${menu[eleccion-1].ingredientes}`)
-            break;
-
-         }
-
-         }
-
-         //console.log(CARRITO);
-         // Pre entrega Nro 1 : utilizacion de metodos de busqueda y filtrado
-          //pedimos al usuario que seleccione un tipo de hamburgesa para el filtrado
-         // const seleccion = prompt(`selecciono un tipo de menu:                  
-         //                   1. hamburgesas simples.
-         //                   2. hamburguesas Dobles.
-         //                   3. hamburgesas Vegeterianas.`)
-
-    
-       // segun la opcion elegida llamamos al metodo filtraTipo que devuelve un el menu con la seleccion
-        
-      //   let tipo="";
-      //    if (seleccion==="1") {                                             
-      //     tipo="simple"  
-      //     filtrarTipo(tipo)      
            
-      //   }
-      //   else if (seleccion==="2") {
+
+              function mostrarCarrito(){
+
+                if (carrito.length > 0) {
+                  
+                 
+                for (let i of carrito) {
+                  
+                      //creamos un card para mostrar el producto que agramos
+                      let card = document.createElement("div");
+                      
+                      //se agrega a las cards las clases "card",
+                      card.classList.add("card");
+                      //cremaos un div imgcontainer
+                      let imgContainer = document.createElement("div");
+                    
+                      
+                      let container = document.createElement("div");
+                      container.classList.add("container");
+                      //nombre producto
+                      let name = document.createElement("h5");
+                      name.classList.add("nombreProducto");
+                      name.innerText = i.nombre.toUpperCase();
+                      container.appendChild(name);
+                      //precio 
+                      let price = document.createElement("h5");
+                      price.innerText = "$" + i.precio;
+                      container.appendChild(price);
+                      card.appendChild(container);
+                  
+                      //descripcion
+                      
+                      card.appendChild(container);
+                      document.getElementById("conteinerCart").appendChild(card);
+            
+
+                }
+               //metodo para calcular el total del carrito de compras
+                const resultado= carrito.reduce((acc, item)=>{
+
+                  return acc + item.precio;
+                },0);
+               // creamos un contenedor para el total del carrito de compras
+                const containerResultado= document.createElement("div");
+                let total = document.createElement("h5");
+                total.classList.add("nombreProducto");
+                total.innerText = "Total a Pagar: " + resultado;
+                containerResultado.appendChild(total);
+                conteinerCart.appendChild(containerResultado);
+              } 
+              else {
+                alert("No hay Productos en el carrito")
+              }
+
+              }
+
+             
+
          
-      //    tipo="doble"  
-      //     filtrarTipo(tipo)   
-      
-      //   }
-      //   else if (seleccion==="3") {
-      //    tipo="vegetariana" 
-      //    filtrarTipo(tipo)   
-      //   }
 
-
-        // capturamos el resultado en "tipo" y se realiza la busqueda con filter y recorremos el array que nos devuelve
-        function filtrarTipo(tipo){
-
-              let resultado = menu.filter(menu => menu.tipo==tipo)     
-             return    resultado.forEach(element => {console.log(element.nombre, element.precio, element.ingredientes.join()) })
-        }
-
-
-
-        //mostrar los elementos en pantalla
-        const mostrarProductos =(menu)=>{
-
-        const contenedor = document.getElementById("cards");
-
-          menu.forEach((menu) => {
-
-         const div = document.createElement("div");
-         div.classList.add("card");
-         div.innerHTML +=`<div class="container">
-                                 <img src=${menu.img} alt="chesse">
-                           
-                                 <div class="details">
-                                 <h3>${menu.nombre}</h3>
-                                 <p>${menu.ingredientes.join()} </p>
-                                 </div>
-                           </div>
-                         `
-        contenedor.appendChild(div)
-        }
-
-      
-
-        )};
-        mostrarProductos(menu);
-       
-       // funcion dark mode
-    
-let darkmode = document.querySelector('#darkmode');
- 
-darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-moon')){
-        darkmode.classList.replace('bx-moon','bx-sun');
-        document.body.classList.add('active');
-    }else{
-        darkmode.classList.replace('bx-sun','bx-moon');
-        document.body.classList.remove('active');
-    }
-}
- 
-
-        
-
+           
 
 
       
